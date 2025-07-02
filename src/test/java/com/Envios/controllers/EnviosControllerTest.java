067a3513983a399e0a2b9c0049726a17147e71cc
natanael reveco <reveconatanael03@gmail.com>
2025-07-02 00:18:36 -0400

    se hizo una prueba con junit

---
+++ src/test/java/com/Envios/controllers/EnviosControllerTest.java
@@ -0,0 +1,25 @@
+package com.Envios.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class EnviosControllerTest {
+    @Test
+    void testCrear() {
+
+    }
+
+    @Test
+    void testEliminar() {
+
+    }
+
+    @Test
+    void testListar() {
+
+    }
+
+    @Test
+    void testObtener() {
+
+    }
+}
